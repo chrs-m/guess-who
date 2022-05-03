f646b3b84d65b2ab3f0f58c1ba0772f24bc31db7
chrs-m <mister.ch@gmail.com>
2022-05-03 09:01:54 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import * as pixi from 'pixi.js';
 
 var app = new pixi.Application({
+  antialias: true,
   width: window.innerWidth,
   height: window.innerHeight,
   backgroundColor: 0x2c3e50,
@@ -9,6 +10,6 @@ document.body.appendChild(app.view);
 
 const gr = new pixi.Graphics();
 gr.beginFill(0xffffff);
-gr.drawCircle(30, 30, 30);
+gr.drawCircle(40, 40, 30);
 gr.endFill();
 app.stage.addChild(gr);
