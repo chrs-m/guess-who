21d2da526a74d51ac74c5a988f3c6e77bc5c7d36
chrs-m <mister.ch@gmail.com>
2022-05-05 12:55:43 +0200

    Show 24 tiles on board

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
-import * as pixi from 'pixi.js';
+import * as PIXI from 'pixi.js';
+const graphics = new PIXI.Graphics();
 
-var app = new pixi.Application({
+var app = new PIXI.Application({
   antialias: true,
   width: window.innerWidth,
   height: window.innerHeight,
@@ -8,8 +9,61 @@ var app = new pixi.Application({
 });
 document.body.appendChild(app.view);
 
-const gr = new pixi.Graphics();
-gr.beginFill(0xffffff);
-gr.drawCircle(40, 40, 30);
-gr.endFill();
-app.stage.addChild(gr);
+const style = new PIXI.TextStyle({
+  fontFamily: 'Montserrat',
+  fontSize: 48,
+  fill: 'deepskyblue',
+  stroke: '#ffffff',
+  strokeThickness: 4,
+  dropShadow: true,
+  dropShadowDistance: 10,
+  dropShadowAngle: Math.PI / 2,
+  dropShadowBlur: 4,
+  dropShadowColor: '#000000',
+});
+
+const myText = new PIXI.Text('Guess Who?!', style);
+myText.x = window.innerWidth / 1.5;
+myText.y = 50;
+myText.interactive = true;
+myText.buttonMode = true;
+let isSet = false;
+myText.on('pointerdown', () => {
+  isSet = !isSet;
+  myText.text = isSet ? 'tjena tjena' : 'Hello world';
+});
+
+app.stage.addChild(myText);
+
+myText.style.wordWrap = true;
+myText.style.wordWrapWidth = 400;
+myText.style.align = 'center';
+
+const cardTexture = PIXI.Texture.from('./assets/img/Character_card.png');
+const cardSprite = new PIXI.TilingSprite(
+  cardTexture,
+  app.screen.width,
+  app.screen.height
+);
+
+cardSprite.tileScale.set(0.5, 0.5);
+app.stage.addChild(cardSprite);
+
+const boxWidth = 100;
+const boxHight = 120;
+const gap = 48;
+for (let x = 0; x < 6; x++) {
+  for (let y = 0; y < 4; y++) {
+    graphics.beginFill(0xde3249);
+    graphics.drawRoundedRect(
+      x * boxWidth + x * gap,
+      y * boxHight + y * gap,
+      100,
+      120,
+      8
+    );
+    graphics.endFill();
+  }
+
+  app.stage.addChild(graphics);
+}
