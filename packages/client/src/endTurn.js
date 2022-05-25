47f94644338bfe08ba2faad2d58009a7992deb0d
chrs-m <mister.ch@gmail.com>
2022-05-25 10:44:33 +0200

    player turns now works

--- packages/client/src/endTurn.js
+++ packages/client/src/endTurn.js
@@ -1,9 +1,16 @@
-import server from './socket';
+import socket from './socket';
 
 let playerTurn = true;
 const blockBox = document.querySelector('#app');
-
 const nextTurnBtn = document.querySelector('#turn');
+
+let player = false;
+
+socket.on('player', (data) => {
+  if (!player) {
+    player = data;
+  }
+});
 
 if (nextTurnBtn !== null) {
   nextTurnBtn.addEventListener('click', () => {
@@ -13,20 +20,26 @@ if (nextTurnBtn !== null) {
 }
 
 const endTurn = () => {
-  server.emit('pass_turn');
-  if (playerTurn === false) {
-    if (blockBox !== null) {
-      blockBox.classList.add('stopOverlay');
-
-      const text = document.createTextNode(
-        'Please wait for oppent to end its turn..'
-      );
-
-      blockBox.appendChild(text);
-      blockBox.appendChild;
-    }
-  }
+  socket.emit('pass_turn', player);
 };
+
+socket.on('turn', (data) => {
+  if (player !== data) {
+    blockBox.classList.add('stopOverlay');
+
+    const text = document.createTextNode(
+      'Please wait for oppent to end its turn..'
+    );
+
+    blockBox.appendChild(text);
+    blockBox.appendChild;
+    blockBox.style.display = 'block';
+  }
+
+  if (player === data) {
+    blockBox.style.display = 'none';
+  }
+});
 
 endTurn();
 
--- packages/server/server.js
+++ packages/server/server.js
@@ -10,27 +10,13 @@ const io = new Server(server, { cors: { origin: '*' } });
 let players = [];
 
 io.on('connection', (socket) => {
-  console.log('A player connected');
+  // console.log('A player connected');
 
   let playerId = socket.id;
 
-  let current_turn = 0;
-  let timeOut;
-  let _turn = 0;
+  socket.emit('player', playerId);
 
-  function next_turn() {
-    _turn = current_turn++ % players.length;
-    players[_turn].emit('your_turn');
-    console.log('next turn triggered ', _turn);
-  }
-
-  function resetTimeOut() {
-    if (typeof timeOut === 'object') {
-      console.log('timeout reset');
-      clearTimeout(timeOut);
-    }
-  }
-
+  //JOIN & CONNECTION LOGIC
   socket.on('join room', (data) => {
     console.log('Player joined room', data);
 
@@ -44,46 +30,38 @@ io.on('connection', (socket) => {
     socket.join('1');
   });
 
+  players.push(playerId);
+  // players.push({ id: playerId, name: "", avatar: '' });
+
+  socket.on('disconnect', function () {
+    console.log('A player disconnected');
+    players = players.filter((p) => {
+      return p != socket.id;
+    });
+  });
+
+  //CHAT LOGIC
   socket.on('send message', (data) => {
     console.log(data);
 
     io.to('1').emit('received message', data);
   });
 
-  players.push(playerId);
+  //TURN LOGIC
+  socket.on('pass_turn', (player) => {
+    let nrOfPlayers = Object.values(players).length;
+    // console.log(player);
 
-  socket.on('disconnect', function () {
-    console.log('A player disconnected');
-    players.splice(players.indexOf(socket), 1);
-    _turn--;
-    console.log('A number of players now ', players.length);
-  });
+    let playerNr = players.findIndex((p) => p == player);
+    // console.log(playerNr);
 
-  console.log(players.length);
-  console.log(players);
+    // console.log({ players });
 
-  ///////////////////////////
-
-  let playerOne = players[0];
-  let playerTwo = players[1];
-
-  if (playerOne === null) {
-    playerOne = playerId;
-  } else {
-    playerTwo = playerId;
-  }
-
-  socket.on('pass_turn', function () {
-    _turn++;
-
-    console.log({ playerOne, playerTwo, _turn });
-    if (players[_turn] == socket) {
-      next_turn();
+    if (playerNr + 1 < nrOfPlayers) {
+      io.emit('turn', players[playerNr + 1]);
+    } else {
+      io.emit('turn', players[0]);
     }
-
-    socket.to('1').emit('next_turn', {
-      player: _turn % 2 == 0 ? playerOne : playerTwo,
-    });
   });
 });
 
